1e2d27b438a8acdd9efa210626b3281297cb2e33
Haneymiya Kazutora <98148880+Nchuuya@users.noreply.github.com>
2022-07-17 18:46:41 +0530

    Update video_hentai.py

--- plugin/video_hentai.py
+++ plugin/video_hentai.py
@@ -7,7 +7,7 @@ import subprocess
 import json
 
 MONGO_URL = os.environ.get("MONGO_URL", None) 
-CACHE_CHANNEL = os.environ.get(int("CACHE_CHANNEL" , "-1001584010815"))
+CACHE_CHANNEL = os.environ.get(int("CACHE_CHANNEL"))
 
 def hentailink(client, callback_query):
     click = callback_query.data
